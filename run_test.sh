ee4dbf472a4921b4f4d3d197b5a9ed07f012b1b3
Peter Geoghegan <peter.geoghegan86@gmail.com>
2015-01-18 04:56:49 +0100

    Correct error message

--- run_test.sh
+++ run_test.sh
@@ -7,7 +7,7 @@ do
 		echo "trying $clients clients:"
 		perl count_upsert.pl $clients 100000 2>&1 | tee -a log
 		if ((${PIPESTATUS[0]} != 0)); then
-			echo "rc $0"
+			echo "Exited from infinite loop due to unexpected error"
 			exit 1
 		fi
 	done
