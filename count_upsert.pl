3ac0449022c7cd83d3b26ff3bec11262ad14b2c2
Peter Geoghegan <peter.geoghegan86@gmail.com>
2015-02-06 16:48:13 -0800

    Default index fillfactor

--- count_upsert.pl
+++ count_upsert.pl
@@ -72,7 +72,7 @@ while (1) {
          $dbh->do(<<'END');
 drop table if exists upsert_race_test;
 create table upsert_race_test(index int, count int) with (fillfactor=100);
-create unique index on upsert_race_test(index) with (fillfactor=100);
+create unique index on upsert_race_test(index) with (fillfactor=90);
 END
     };
     my $dat = $dbh->selectall_arrayref("select index, count from upsert_race_test");
--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ do
 	for clients in 8 16 64 128
 	do
 		echo "trying $clients clients:"
-		perl count_upsert.pl $clients 100000 2>&1 | tee -a log
+		perl count_upsert.pl $clients 1000 2>&1 | tee -a log
 		if ((${PIPESTATUS[0]} != 0)); then
 			echo "Exited from infinite loop due to unexpected error"
 			exit 1
