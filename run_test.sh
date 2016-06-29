e9a6562b3b160834a1a13187fb20943d147db8f9
Peter Geoghegan <pg@bowt.ie>
2016-06-29 15:34:53 -0700

    Don't use overly complicated filler function

--- run_test.sh
+++ run_test.sh
@@ -12,4 +12,14 @@ do
 			exit 1
 		fi
 	done
+	# Exclusion constraints (see README for instructions)
+	for clients in 8 16 64 128
+	do
+		echo "trying $clients clients:"
+		perl count_upsert_exclusion.pl $clients 1000 2>&1 | tee -a log
+		if ((${PIPESTATUS[0]} != 0)); then
+			echo "Exited from infinite loop due to unexpected error"
+			exit 1
+		fi
+	done
 done
